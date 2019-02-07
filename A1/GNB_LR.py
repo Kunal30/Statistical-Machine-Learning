bc52933aa8e8ab212ab4016b2c32cb955668ab4f
Kunal30 <kunalvsuthar@gmail.com>
2019-02-07 12:35:34 -0700

    Altered a bit of shuffling

--- A1/GNB_LR.py
+++ A1/GNB_LR.py
@@ -14,7 +14,7 @@ def main():
     #to get the get the data for the 6 fractions over 5 runs
 	
 	train_test_data,frac_values_per_dataset= three_fold_cross_validation(X,Y,[0.01,0.02,0.05,0.1,0.625,1]) 
-	
+	print(train_test_data)
 	#Now we can train the Gaussian Naive Bayes classifier
 	#for all training set sizes
 	hash_map_accuracies={.01: [], 0.02: [], 0.05: [], 0.1: [], 0.625: [], 1:[] } 
@@ -51,7 +51,7 @@ def main():
 		X_test=train_test_data[i][2]
 		Y_test=train_test_data[i][3]				
 		
-		W,b= train_LR_classifier(X_train,Y_train,0.01)
+		W,b= train_LR_classifier(X_train,Y_train,0.05)
 		predictions= predict_LR_classifier(X_test,W,b)
 		accuracy= calc_accuracy(predictions,Y_test)
 		hash_map_accuracies[frac_values_per_dataset[i]].append(accuracy)
@@ -74,6 +74,7 @@ def plot_graph(plot1,plot2,title):
 	plt.title(title)
 	plt.plot(X,Y1,'r',label='GNB')
 	plt.plot(X,Y2,'b',label='LR')
+	# plt.yticks(np.arange(min(Y1),1, step=0.1))
 	plt.ylabel('Accuracy')
 	plt.xlabel('Training Data Size')
 	plt.legend()		
@@ -88,7 +89,7 @@ def train_LR_classifier(X_train, Y_train, eta):
 
 	W=np.zeros((rows,1))
 	b=0
-	for i in range(0,100):
+	for i in range(0,10):
 		Z=np.dot(W.T,X_train)+b
 		Y_hat=sigmoid(Z)
 		loss=Y_train-Y_hat
@@ -132,8 +133,7 @@ def predict_LR_classifier(X_test,W,b):
 			predictions.append(1)
 		else:
 			predictions.append(0)	
-
-	# print(predictions)		
+			
 	return predictions
 def predict_GNB_classifier(X_test, parameters):
 	
@@ -172,7 +172,7 @@ def calc_accuracy(predictions, Y_test):
 	accuracy_matrix[predictions == Y_test]=1
 	accuracy_matrix[predictions != Y_test]=0
 
-	return (((sum(accuracy_matrix))*1.00)/len(Y_test))*100
+	return (((sum(accuracy_matrix))*1.00)/len(Y_test))
 
 
 def three_fold_cross_validation(X,Y,fractions):
@@ -190,12 +190,17 @@ def three_fold_cross_validation(X,Y,fractions):
 
 	for fraction in fractions:		
 		#randomization starts here
-		state = np.random.get_state()
-		X=np.take(X,np.random.permutation(X.shape[0]),axis=0,out=X)
-		np.random.set_state(state)
-		Y=np.take(Y,np.random.permutation(Y.shape[0]),axis=0,out=Y)
+		state=random.getstate()
+		X=random.sample(X,X.shape[0])
+		random.setstate(state)
+		Y=random.sample(Y,Y.shape[0])
+		# state = np.random.get_state()
+		# X=np.take(X,np.random.permutation(X.shape[0]),axis=0,out=X)
+		# np.random.set_state(state)
+		# Y=np.take(Y,np.random.permutation(Y.shape[0]),axis=0,out=Y)
 		#randomization ends here
-
+		X=np.asarray(X)
+		Y=np.asarray(Y)
 		#now splitting the data into 3 folds
 		X_temp=np.split(X,3)
 		Y_temp=np.split(Y,3)
@@ -218,16 +223,25 @@ def three_fold_cross_validation(X,Y,fractions):
 				value=math.floor(fraction*Y_TRAIN.shape[0])
 				frac_values_per_dataset.append(fraction)
 				# print(X_TRAIN.shape)				
-				X_TRAIN_TEMP= X_TRAIN[:int(value)]
-				Y_TRAIN_TEMP= Y_TRAIN[:int(value)]
-				
+				# X_TRAIN_TEMP= X_TRAIN[:int(value)]
+				# Y_TRAIN_TEMP= Y_TRAIN[:int(value)]
+				state=random.getstate()
+				X_TRAIN_TEMP=random.sample(X_TRAIN,int(value))
+				random.setstate(state)
+				Y_TRAIN_TEMP=random.sample(Y_TRAIN,int(value))
+				X_TRAIN_TEMP=np.asarray(X_TRAIN_TEMP)
+				Y_TRAIN_TEMP=np.asarray(Y_TRAIN_TEMP)
 				train_test_data.append([X_TRAIN_TEMP,Y_TRAIN_TEMP,X_TEST,Y_TEST])
 
 				#now randomizing the training data and label
-				state = np.random.get_state()
-				X_TRAIN=np.take(X_TRAIN,np.random.permutation(X_TRAIN.shape[0]),axis=0,out=X_TRAIN)
-				np.random.set_state(state)				
-				Y_TRAIN=np.take(Y_TRAIN,np.random.permutation(Y_TRAIN.shape[0]),axis=0,out=Y_TRAIN)
+				# X_TRAIN=random.sample(X_TRAIN,X_TRAIN.shape[0])
+				# Y_TRAIN=random.sample(Y_TRAIN,Y_TRAIN.shape[0])
+				# X_TRAIN=np.asarray(X_TRAIN)
+				# Y_TRAIN=np.asarray(Y_TRAIN)
+				# state = np.random.get_state()
+				# X_TRAIN=np.take(X_TRAIN,np.random.permutation(X_TRAIN.shape[0]),axis=0,out=X_TRAIN)
+				# np.random.set_state(state)				
+				# Y_TRAIN=np.take(Y_TRAIN,np.random.permutation(Y_TRAIN.shape[0]),axis=0,out=Y_TRAIN)
 
 	return np.array(train_test_data),frac_values_per_dataset
 
