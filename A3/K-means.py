5868c1b3f6a4c9d181c163dd0cffdf351e9e9a35
Kunal30 <kunalvsuthar@gmail.com>
2019-04-02 20:11:00 -0700

    Finishedgit add .

--- A3/K-means.py
+++ A3/K-means.py
@@ -21,12 +21,22 @@ def main():
 	#Extracting the data from the file	
 	data=pd.read_csv('bc.txt', sep=",", header=None)	
 	data=data.values[:,1:10]
-	K=[8]
+	K=[2,3,4,5,6,7,8]
 	potential=[]
 	print(data.shape)
 	for i in K:
 		potential.append(apply_KMeans(i,data))
 
+	plotgraph(potential,K)	
+
+def plotgraph(potential,K):
+	fig= plt.figure()
+	plt.title('K-Means: Potential Value vs K')
+	plt.plot(K,potential,'r',label='Potential Function')	
+	plt.ylabel('Potential Value')
+	plt.xlabel('Value of K')
+	plt.legend()		
+	plt.show()		
 
 
 def apply_KMeans(K,data):
@@ -39,17 +49,26 @@ def apply_KMeans(K,data):
 	cluster_class=None
 	new_centroids=data[rand_k]
 	
-	for i in range(0,1):
+	for i in range(0,1000):
 		print("Iteration---------------->",i)
 		cluster_class=reassign_centroid(new_centroids,data)
 		prev_centroids=new_centroids
 		new_centroids=recompute_new_centroids(K,cluster_class,data)
 
-	potential=compute_PotentialFunction(new_centroids,data,cluster_class)
+	potential=calculate_PotentialFunction(new_centroids,data,cluster_class)
 
+	return potential
 
-def compute_PotentialFunction(new_centroids,data,cluster_class):
-	
+def calculate_PotentialFunction(new_centroids,data,cluster_class):
+	potential=0
+	for i in range(0,699):
+		diff=data[i]-new_centroids[cluster_class[i]]
+		sq=np.square(diff)
+		sum_t=np.sum(sq)
+		potential=potential+sum_t
+
+	# print(potential)	
+	return potential	
 
 
 # Euclidean Distance 
@@ -66,7 +85,9 @@ def recompute_new_centroids(K,cluster_class,data):
 			if(cluster_class[j]==i):
 				sum_t=sum_t+data[j]
 				points=points+1
-		centroids.append(sum_t*1.0/points)
+		
+		if(points!=0):			
+			centroids.append(sum_t*1.0/points)
 	
 	centroids=np.asarray(centroids)
 
